941a574d1b25910d2271dce0505e8aa9c794fb32
Vitorio Almeida <vitorio.almeida@hotmail.com>
2024-08-05 20:38:46 -0300

    Added two difficulties and made the main menu

---
+++ easyGame.py
@@ -0,0 +1,41 @@
+import random
+
+
+# Randomly select an operator and a random number between 1 and 100
+def math_Easy_Generator():
+    operators = ['+', '-']
+    operator = random.choice(operators)
+
+    num1 = random.randint(1, 50)
+    num2 = random.randint(1, 50)
+
+    return num1, num2, operator
+
+
+# Calculate the correct answer based on the operator
+def math_Easy_answer(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+
+
+def math_Game_E():
+    print("Welcome to the math game at the easy difficulty level. You will be presented with questions involving addition and subtraction, which may include negative number results.")
+    points = 0
+
+    while True:
+        num1, num2, operator = math_Easy_Generator()
+        correct_answer = math_Easy_answer(num1, num2, operator)
+
+        print(f"What is {num1} {operator} {num2}?")
+        user_answer = int(input("Answer: "))
+
+        if user_answer == correct_answer:
+            print("Correct!")
+            points += 1
+        else:
+            print(f"Incorrect! The correct answer is {correct_answer}.")
+            break
+
+    print(f"You scored {points} points.")
--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print('Hello, World!')
-print (1 + 2)
+from basicGame import *
+from easyGame import *
+
+# Display a welcome message and prompt the user to select a difficulty level
+print("Welcome to the Math Game!")
+print("Please select the difficulty level:")
+print("1. Basic")
+print("2. Easy")
+print("3. Medium")
+print("4. Hard")
+
+# Prompt the user to select a difficulty level and validate the input
+
+choice = int(input("Enter the number corresponding to your choice (1-4): "))
+if choice == 1:
+    math_Game_B()
+if choice == 2:
+    math_Game_E()
