3f2bed08b4603d2e2a8af69a1715fee208677e69
Vitorio Almeida <vitorio.almeida@hotmail.com>
2024-08-13 13:07:04 -0300

    I removed comments (unecessary)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from basicGame import *
 from easyGame import *
+from mediumGame import *
 
-# Display a welcome message and prompt the user to select a difficulty level
 print("Welcome to the Math Game!")
 print("Please select the difficulty level:")
 print("1. Basic")
@@ -9,10 +9,10 @@ print("2. Easy")
 print("3. Medium")
 print("4. Hard")
 
-# Prompt the user to select a difficulty level and validate the input
-
 choice = int(input("Enter the number corresponding to your choice (1-4): "))
 if choice == 1:
     math_Game_B()
 if choice == 2:
     math_Game_E()
+if choice == 3:
+    math_Game_M()
